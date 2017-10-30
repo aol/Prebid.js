fd3b2b75093971cf3cb48811ca1e77cc87e34660
vzhukovsky <valentin.zhukovsky@oath.com>
2017-10-30 14:21:24 +0300

    Implemented AOL user syncs config via setConfig API.

--- src/adapters/bidderFactory.js
+++ src/adapters/bidderFactory.js
@@ -181,13 +181,13 @@ export function newBidder(spec) {
       // After all the responses have come back, fill up the "no bid" bids and
       // register any required usersync pixels.
       const responses = [];
-      function afterAllResponses(request) {
+      function afterAllResponses() {
         fillNoBids();
         if (spec.getUserSyncs) {
           let syncs = spec.getUserSyncs({
             iframeEnabled: config.getConfig('userSync.iframeEnabled'),
             pixelEnabled: config.getConfig('userSync.pixelEnabled'),
-          }, responses, request);
+          }, responses);
           if (syncs) {
             if (!Array.isArray(syncs)) {
               syncs = [syncs];
@@ -292,7 +292,7 @@ export function newBidder(spec) {
               addBidUsingRequestMap(bids);
             }
           }
-          onResponse(request);
+          onResponse();
 
           function addBidUsingRequestMap(bid) {
             const bidRequest = bidRequestMap[bid.requestId];
