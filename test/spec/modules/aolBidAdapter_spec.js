d3f8d5c1483cb870df8bcc62dbbe2d59d9cc8fcb
vzhukovsky <valentin.zhukovsky@oath.com>
2018-01-26 09:54:15 +0300

    Improved unit tests description.

--- test/spec/modules/aolBidAdapter_spec.js
+++ test/spec/modules/aolBidAdapter_spec.js
@@ -535,7 +535,7 @@ describe('AolAdapter', () => {
   });
 
   describe('_formatPixels()', () => {
-    it('should return pixels wrapped with a flag that allow to drop them only once', () => {
+    it('should return pixels wrapped for dropping them once and within nested frames ', () => {
       let pixels = '<script>document.write(\'<pixels-dom-elements/>\');</script>';
       let formattedPixels = spec._formatPixels(pixels);
 
